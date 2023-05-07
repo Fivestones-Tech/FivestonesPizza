422e5ca4b1513eb53b442ebc8637a0de70441486
ogunnaike folawe matthew <fivestonestech@gmail.com>
2023-05-08 00:09:30 +0100

    Nav Menu
    Adding JavaScript to the navigation menu to open the navigation list

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 
-const navMenu = document.getElementById('nav-menu')
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');  
+const navLink = document.querySelectorAll('.nav__link');
+
+
+
 ////////////////////////TOGGLE MENU///////////////////
 //////MENU SHOW////////////
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
 
 /////////////MENU HIDDEN///////////////
 
